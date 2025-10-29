feaeaadd4541806f1796d9456ac43c59065b0635
Siddhivinayak06 <siddhivinayaksawant04@gmail.com>
2025-10-29 21:58:01 +0530

    BookIt Done

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,6 @@ import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['images.unsplash.com', 'images.pexels.com', process.env.NEXT_PUBLIC_SUPABASE_BUCKET_HOST ?? ''],
   },
